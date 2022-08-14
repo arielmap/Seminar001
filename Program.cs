54f97a1030019b41110e2dca366488a1a6bf0823
Vladislav izmailov <arielmap@icloud.com>
2022-08-14 19:20:41 +0300

    seminar001

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿// int string bool float double
+// int a = 10;
+// string = "hello"
+// bool = true // false
+// flat d = 15.3   ==== 10 млн значений
+// double e = 15.4f; === 100 млн значений
+// Console.WriteLine("Введите первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Ваш ответ");
+// Console.WriteLine(number1 + number2);
+
+
+
+// Console.WriteLine("Введите первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите оператор +/-/*/:");
+// string Operator = Console.ReadLine();
+// if(Operator == "+")
+// {
+//     Console.WriteLine(number1 + number2);
+// }
+// else if(Operator == "-")
+// {
+//     Console.WriteLine(number1 - number2);
+// }
+// else if(Operator == "*")
+// {
+//     Console.WriteLine(number1 * number2);
+// }
+// else if(Operator == ":")
+// {
+//     Console.WriteLine(number1 / number2);
+// }
+
+
+// while (true)
+// {
+//     Console.WriteLine("Введите первое число");
+//     int number1 = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите второе число");
+//     int number2 = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите оператор +/-/*/:");
+//     string Operator = Console.ReadLine();
+//     if (Operator == "+")
+//     {
+//         Console.WriteLine(number1 + number2);
+//     }
+//     else if (Operator == "-")
+//     {
+//         Console.WriteLine(number1 - number2);
+//     }
+//     else if (Operator == "*")
+//    {
+//         Console.WriteLine(number1 * number2);
+//     }
+//     else if (Operator == ":")
+//     {
+//        Console.WriteLine(number1 / number2);
+//     }
+//     else if (Operator == "end")
+//     {
+//         break;
+//         }
+// }
+
+
+
+// Homework
+// Первая задача
+
