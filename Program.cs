1fbde256a7b29d22298b971fb6b2b794985d9a4d
Vladislav izmailov <arielmap@icloud.com>
2022-08-14 20:45:55 +0300

    homework

--- Program.cs
+++ Program.cs
@@ -72,3 +72,52 @@
 // Homework
 // Первая задача
 
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine(number1);
+}
+else if (number1 < number2)
+{
+    Console.WriteLine(number2);
+}
+
+// Вторая задача
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine(number1);
+}
+if (number2 > number1 && number2 > number3)
+{
+    Console.WriteLine(number2);
+}
+if (number3 > number1 && number3 > number2)
+{
+    Console.WriteLine(number3);
+}
+
+// Третья задача
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else 
+{
+    Console.WriteLine("Число является нечетным");
+}
+
+// Четвертая задача(не могу понять как ее написать)
+
